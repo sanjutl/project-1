cf5cee2ac53bcaca5032b84d30a285f3d10edfdf
sanju <sanjutl19@gmail.com>
2024-08-23 12:46:47 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showNav(){
+    const nav=document.getElementById("a")
+    nav.style.display="flex"
+}
+function hideNav(){
+    const nav=document.getElementById("a")
+    nav.style.display="none"
+}
